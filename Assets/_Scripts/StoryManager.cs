b82e3bd279dd603688987d44861a13e8ae0fb173
Anggitaseagull <anggitaramanda1295@gmail.com>
2026-05-21 02:57:27 +0700

    2

--- Assets/_Scripts/StoryManager.cs
+++ Assets/_Scripts/StoryManager.cs
@@ -9,15 +9,16 @@ public class StoryManager : MonoBehaviour
 
     public RawImage backgroundImage;
     public TMP_Text storyText;
+    public TMP_Text nextButtonText;
 
     public Texture[] backgrounds;
 
     string[] story =
     {
-        "Di sebuah dunia gelap...",
-        "Cahaya mulai muncul...",
-        "Cahaya mulai muncul...",
-        "Petualangan dimulai..."
+        "Di sebuah desa, terdapat remaja bernama Taher yang memiliki kepribadian yang buruk. Taher tidak pernah mendengarkan nasehat yang diberikan ibunya dan selalu menjawab.",
+        "Hingga di suatu hari, ibu Taher tidak lagi memiliki kesabaran untuk menasehati Taher dan menyeret Taher keluar rumah.",
+        "Dengan emosi, Ibu mengusir Taher dari rumah, Ibu berharap, anaknya akan berubah menjadi lebih baik.",
+        "Taher yang terbiasa mendapat kecukupan, kini merasa khawatir setelah diusir. Akhirnya, Taher bertekad untuk berubah menjadi pribadi yang lebih baik."
     };
 
     int index = 0;
@@ -45,8 +46,18 @@ public class StoryManager : MonoBehaviour
     }
 
     void ShowPage()
+{
+    backgroundImage.texture = backgrounds[index];
+    storyText.text = story[index];
+
+    // Kalau halaman terakhir
+    if(index == story.Length - 1)
     {
-        backgroundImage.texture = backgrounds[index];
-        storyText.text = story[index];
+        nextButtonText.text = "Selesai";
     }
+    else
+    {
+        nextButtonText.text = "Lanjut";
+    }
+}
 }
